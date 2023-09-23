4958ab7f4b7ae39e46a9b77a9ad240b2f65fe95e
twatanabe <email@takumiwatanabe.me>
2023-09-22 18:59:01 -0700

    Fix general padding and alignments

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -7,11 +7,13 @@ const Footer = ({ style = {} }) => {
     <div style={style} className="footer-container">
       <div className="footer-container-content">
         <p className="site-details">
+          Abstractly designed in <a href="https://www.figma.com/">Figma</a> {" "} and styled with <a href="https://lesscss.org/">Less</a> compiler.
+          <br />
           Developed in <a href="https://react.dev/">React.js</a> using {" "}
           <a href="https://www.vim.org/">Vim</a> and {" "}
           <a href="https://code.visualstudio.com/">Visual Studio Code</a>.
           <br />
-          Deployed with <a href="https://vercel.com/">Vercel</a>.
+          Deployed with <a href="https://vercel.com/">Vercel</a> combining domain access via <a href="https://porkbun.com/">Porkbun</a>.
         </p>
         <p className="watermark">
           © TAKUMI WATANABE • <a href="takumiwatanabe.me">TAKUMIWATANABE.ME</a>
